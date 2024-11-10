165ee8b458702110ab73f3ef1006255b5dff0b51
MarcusVibeH <36028528+MarcusVibeH@users.noreply.github.com>
2024-11-10 18:26:55 +0100

    added startup for postgress

---
+++ JPLearning/JPLearningREST/Data/JPLearningDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JPLearningREST.Data
+{
+    public class JPLearningDbContext : DbContext
+    {
+        public JPLearningDbContext(DbContextOptions<JPLearningDbContext> options) : base(options) { }
+
+        
+    }
+}
